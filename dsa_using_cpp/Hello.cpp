5ba94d0357adaf02959b64a71ba3e3c0a9f8f8db
Chinmay Jaiswal <chinmayjaiswalcj@gmail.com>
2022-04-23 00:47:05 +0530

    adds hello world example using c++ | Chinmay

---
+++ dsa_using_cpp/Hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(void){
+    std::cout << "Hello World!! Hakuna Matata!!!";
+}
+
+/*
+TODO:
+    1. C pointers workshop from youtube
+    2. session 1 video  from youtube link
+    3. session 2 video from youtube link
+         -> array_of_ptrs_to_int.c
+         -> dynamic_array_of_ptrs_to_dynamic_integers.c
+                --> has exercise in this file
+         ->
+    4. Session 3
+            exercise1: input_unspecified_numbers_of_integers
+            exercise2: vector_int
+            exercise3: vector_built_in_data_type
+            exercise4: vector_user_defined_data_type
+
+*/
